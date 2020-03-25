084974e93de1ac79536cb760ca41ff30e7aca10c
Andres Elizondo <andres.e@adestech.mx>
2020-03-25 14:03:10 -0600

    Convert to tf2
    * Convert all to tf2 using tf_upgrade_v2.
    * Upgrade TF to 2.1, Keras to 2.3.1 and remove pocketsphinx.
    * Adjusts training parameters for 20 units in GRU. Adds early stop, reduce learning rate. Removes Tensorboard due to compat issues.
    * Adds TFLiteRunner with enough compatility for inference.
    * Bugfix: Increments counter for each prediction.
    * Adjusts KerasRunner for compat with tf2.
    * Sets TF version to 2.2.0rc2. Update when 2.2.0 is released.
    * Adapts convert script for tf2 and tflite.
    * Changes precise-convert default output file extension to tflite.
    * General cleanup for merge.

--- precise/scripts/train_generated.py
+++ precise/scripts/train_generated.py
@@ -92,7 +92,7 @@ class TrainGeneratedScript(BaseScript):
         self.model = create_model(args.model, params)
         self.listener = Listener('', args.chunk_size, runner_cls=lambda x: None)
 
-        from keras.callbacks import ModelCheckpoint, TensorBoard
+        from keras.callbacks import ModelCheckpoint
         checkpoint = ModelCheckpoint(args.model, monitor=args.metric_monitor,
                                      save_best_only=args.save_best)
         epoch_fiti = Fitipy(splitext(args.model)[0] + '.epoch')
@@ -105,9 +105,8 @@ class TrainGeneratedScript(BaseScript):
         self.model_base = splitext(self.args.model)[0]
 
         self.callbacks = [
-            checkpoint, TensorBoard(
-                log_dir=self.model_base + '.logs',
-            ), LambdaCallback(on_epoch_end=on_epoch_end)
+            checkpoint,
+            LambdaCallback(on_epoch_end=on_epoch_end)
         ]
 
         self.data = TrainData.from_both(args.tags_file, args.tags_folder, args.folder)
@@ -236,7 +235,7 @@ class TrainGeneratedScript(BaseScript):
                 callbacks=self.callbacks, initial_epoch=self.epoch
             )
         finally:
-            self.model.save(self.args.model)
+            self.model.save(self.args.model, save_format='h5')
             save_params(self.args.model)
 
 
